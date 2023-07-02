99bb6a191ac6964091bd0054b186feb1042774aa
Rizki Akbar <rizkyakbar1511@gmail.com>
2023-07-02 17:47:27 +0700

    update icon size

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center min-h-full py-12 bg-gray-100 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <Image className="w-auto mx-auto" src="/images/logo.png" width={48} height={48} alt="logo" />
+        <Image className="w-auto mx-auto" src="/images/logo.png" width={72} height={72} alt="logo" />
         <h2 className="mt-6 text-3xl font-bold tracking-tight text-center text-gray-900">Sign in to your account</h2>
       </div>
       <AuthForm />
