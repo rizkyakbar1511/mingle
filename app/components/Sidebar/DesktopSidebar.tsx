1b7362de86dad068314ffcc641b55cf08fa5296f
Rizki Akbar <rizkyakbar1511@gmail.com>
2023-07-05 00:03:04 +0700

    remove console

--- app/components/Sidebar/DesktopSidebar.tsx
+++ app/components/Sidebar/DesktopSidebar.tsx
@@ -14,8 +14,6 @@ const DesktopSidebar: React.FC<DesktopSidebarProps> = ({ currentUser }) => {
   const routes = useRoutes();
   const [isOpen, setIsOpen] = useState(false);
 
-  console.log(currentUser);
-
   return (
     <>
       <SettingsModal currentUser={currentUser} isOpen={isOpen} onClose={() => setIsOpen(false)} />
